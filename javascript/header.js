73d8987347d8266a067c9acf415350eacda42d39
redmoon9156 <98579460+redmoon9156@users.noreply.github.com>
2026-04-08 14:06:55 +0900

    Update header.js

--- javascript/header.js
+++ javascript/header.js
@@ -1,9 +1,7 @@
-// header.js
 function toggleMenu() {
     const nav = document.getElementById('navOverlay');
     const spans = document.querySelectorAll('.menu-trigger span');
     nav.classList.toggle('active');
-
     if(nav.classList.contains('active')) {
         spans[0].style.transform = 'translateY(8.5px) rotate(45deg)';
         spans[1].style.opacity = '0';
